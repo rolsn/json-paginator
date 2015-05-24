922d20d155826cf3d8b01f0644d61ce3b1506b5b
rj <rj@eth3.org>
2015-05-24 04:39:06 -0400

    returns paginated data

--- server.js
+++ server.js
@@ -5,32 +5,63 @@ var url = require('url');
 
 // script options
 var libPath = './';
+var queryParams = {
+        page: 'page',
+        pageSize: 'count'
+};
+var requestedPage = 1;
+var pageSize = 10;
 
 console.log("starting server...");
 
 var server = http.createServer(function (req, res) {
     var q = url.parse(req.url, true).query;
-    var fileRequest = req.url.match(/\/(.*\.json$)/);
+    var fileRequest = req.url.match(/\/(.*\.json)/);
 
     console.log("request: url %s, page # %s", req.url, q.page);
-
-    /*
-     * remember: fileRequest is false if it doesn't match the json regex
-     */
 
     if (fileRequest) {
         var fileName = fileRequest[1];
 
         try {
-            fileStats = fs.lstatSync(libPath + fileName);
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end(fs.readFileSync(libPath + fileName));
+            var fileStats = fs.lstatSync(libPath + fileName);
+            var fileBody = fs.readFileSync(libPath + fileName);
         }
+        // exit with 404 if the file doesn't exist
         catch (e) {
             res.writeHead(404, {'Content-Type': 'text/html'});
             res.end(fileName + ": file not found");
             return
         }
+
+        var params = Object.keys(q);
+
+        // exit with 200 if no params, and display the entire json
+        if (params.length < 1) {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(fileBody);
+            return
+        }
+
+        var json = JSON.parse(fileBody);
+        var totalResults = json.length;
+
+        if (params.indexOf(queryParams.page) > -1) requestedPage = parseInt(q[queryParams.page])
+        if (params.indexOf(queryParams.pageSize) > -1) pageSize = parseInt(q[queryParams.pageSize])
+
+        var fullPages = Math.floor(totalResults/pageSize);
+        if (fullPages >= 1) {
+            var remainder = totalResults % (fullPages * pageSize);
+            var offset = pageSize * (requestedPage - 1);
+            var result = json.slice(offset, offset + pageSize);
+
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(result));
+            return
+        }
+
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end("something went wrong");
 
     }
     else {
